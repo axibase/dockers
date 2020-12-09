172d1d70a90a7298de2226008461cdc6fc3c9e3a
Igor <igor.shmagrinsky@axibase.com>
2020-12-09 10:22:01 +0300

    simplify

--- entrypoint-api-test.sh
+++ entrypoint-api-test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 if [ -z "${axiname}" ] || [ -z "${axipass}" ]; then
-	echo "axiname or axipass is empty. Fail to start container."
-	exit 1
+  echo "axiname or axipass is empty. Fail to start container."
+  exit 1
 fi
-
 
 DISTR_HOME="/opt/atsd"
 UPDATELOG="$(readlink -f ${DISTR_HOME}/logs/update.log)"
@@ -13,26 +12,19 @@ LATEST="atsd.standalone.latest.tar.gz"
 LATESTTAR="${DISTR_HOME}/bin/atsd.standalone.latest.tar.gz"
 revisionFile="applicationContext-common.xml"
 
-function logger {
-    echo "$1" | tee -a $UPDATELOG
+function logger() {
+  echo "$1" | tee -a $UPDATELOG
 }
-
 
 yes | bash ${DISTR_HOME}/bin/update.sh
 
 #check timezone
 if [ -n "${timezone}" ]; then
-    echo "export JAVA_PROPERTIES=\"-Duser.timezone=$timezone \$JAVA_PROPERTIES\"" >> /opt/atsd/conf/atsd-env.sh
+  echo "export JAVA_PROPERTIES=\"-Duser.timezone=$timezone \$JAVA_PROPERTIES\"" >>/opt/atsd/conf/atsd-env.sh
 fi
-rm -rf atsd.standalone.tar.gz
-rm -rf /opt/atsd/bin/atsd
-rm /opt/atsd/conf/license/*
-rm /opt/atsd/logs/atsd.log
-rm /opt/atsd/logs/command*.log
-rm /opt/atsd/logs/err.log
 
-curl https://raw.githubusercontent.com/axibase/atsd/master/rule-engine/resources/calendars/usa.json > /opt/atsd/conf/calendars/usa.json
-curl https://raw.githubusercontent.com/axibase/atsd/master/rule-engine/resources/calendars/rus.json > /opt/atsd/conf/calendars/rus.json
+curl https://raw.githubusercontent.com/axibase/atsd/master/rule-engine/resources/calendars/usa.json >/opt/atsd/conf/calendars/usa.json
+curl https://raw.githubusercontent.com/axibase/atsd/master/rule-engine/resources/calendars/rus.json >/opt/atsd/conf/calendars/rus.json
 logger "USA and RUS workday calendars updated"
 
 ${DISTR_HOME}/bin/atsd-tsd.sh start
@@ -41,5 +33,5 @@ curl -i --data "userBean.username=$axiname&userBean.password=$axipass&repeatPass
 curl -F "file=@/opt/atsd/rules.xml" -F "auto-enable=true" -F "replace=true" http://"$axiname":"$axipass"@127.0.0.1:8088/rules/import
 
 while true; do
- sleep 5
+  sleep 5
 done
